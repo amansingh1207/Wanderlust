95c9addcedb7e9f298a7ab324afa04ab72b527f4
Aman Singh <amansingh9919945944@gmail.com>
2026-03-28 02:35:45 +0530

    fixed atlasdb url

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb+srv://Aman_Singh:Aman2006@cluster0.tn5mi3p.mongodb.net/wanderlust?appName=Cluster0";
 
 main()
     .then(() => {
